defbe81dc79df82c9606d50556e3229bac0f8c8f
sungy <sungy@uway.cn>
2017-03-29 11:18:49 +0800

    3.29第一次提交 11.18

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by SunGuoYan on 17/3/29.
+//  Copyright © 2017年 SunGuoYan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
